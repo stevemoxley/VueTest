6f42cdba700c924f1e3a57158ea094d7820dd0fc
stevemoxley <sdmfyc@gmail.com>
2020-12-15 16:15:17 -0600

    Init

---
+++ VueTest.Web/webpack.config.js
@@ -0,0 +1,29 @@
+﻿var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './wwwroot/js/site.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'site.bundle.js'
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
